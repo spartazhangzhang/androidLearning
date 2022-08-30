815fbbfdc0e0e9f393123a187909a614c9f8f937
张玳辉 <daihui.zhang@colasoft.com.cn>
2022-08-30 17:26:44 +0800

    ch1 init

---
+++ app/src/main/java/com/dh/quiz/MainActivity.java
@@ -0,0 +1,40 @@
+package com.dh.quiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTrueButton = findViewById(R.id.true_button);
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.correct, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 0);
+                toast.show();
+            }
+        });
+
+        mFalseButton = findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(MainActivity.this, R.string.incorrect, Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.TOP, 0, 1);
+                toast.show();
+            }
+        });
+    }
+}
